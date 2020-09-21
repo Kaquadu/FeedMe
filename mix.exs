89dac8c097b8585a78f5bb765edeff3066292b79
Kuba Kowalczykowski <kkowalczykowski@prograils.com>
2020-09-21 15:17:51 +0200

    Update deps

--- mix.exs
+++ mix.exs
@@ -33,15 +33,15 @@ defmodule Feed.MixProject do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
-      {:phoenix, "~> 1.4.9"},
-      {:phoenix_pubsub, "~> 1.1"},
-      {:phoenix_ecto, "~> 4.0"},
-      {:ecto_sql, "~> 3.1"},
+      {:phoenix, "~> 1.5.4", override: true},
+      {:phoenix_pubsub, "~> 2.0"},
+      {:phoenix_ecto, "~> 4.1.0"},
+      {:ecto_sql, "~> 3.4.5"},
       {:postgrex, ">= 0.0.0"},
       {:phoenix_html, "~> 2.11"},
       {:phoenix_live_reload, "~> 1.2", only: :dev},
       {:gettext, "~> 0.11"},
-      {:jason, "~> 1.0"},
+      {:jason, "~> 1.0", override: true},
       {:plug_cowboy, "~> 2.0"},
       {:bcrypt_elixir, "~> 2.0"},
       {:bamboo, "~> 1.4", override: true},
